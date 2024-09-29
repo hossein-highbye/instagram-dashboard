4f103d5f2dacc1a6376d87ea117f713061b13966
Hossein Barzegari <hosseinbarzegari1414@gmail.com>
2024-09-29 17:24:41 +0330

    initial commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+// Database conf
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'instagram_4berry');
+define('DB_USER', '4berryIncINs');
+define('DB_PASS', '*C^5Cf8Ly3ccj1FZ#I');
+
+// facebook app cred
+define('FB_APP_ID', '879373290804025');
+define('FB_APP_SECRET', 'a5692656cc30d758bb5e40766b4e813b');
+define('FB_REDIRECT_URL', 'your_redirect_url');
---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+
+// main conf
+$dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME;
+try {
+    $pdo = new PDO($dsn, DB_USER, DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
